320cfb4a9dce78ef8fd4b9688044e8039492a629
Oliver Swindles <86194326+oliver-swindles@users.noreply.github.com>
2025-02-11 12:18:38 +0000

    Corrected spelling error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <div class="p-12">
   <p class="underline text-sm">useful links</p>
   <ul class="text-xl flex flex-col gap-2 py-2">
-    <li>&gt;<a href="https://www.linkedin.com/in/oiver-swindles/" class="hover:underline">linkedin</a></li>
+    <li>&gt;<a href="https://www.linkedin.com/in/oliver-swindles/" class="hover:underline">linkedin</a></li>
     <li>&gt;<a href="https://spacefrogstudio.com" class="hover:underline">space frog studio</a></li>
   </ul>
 </div>
